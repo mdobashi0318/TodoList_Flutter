d37e99e60b64f8a48ad23abc578af3eb4c07100c
MasaharuDobashi <dobashi.masaharu@gmail.com>
2021-05-04 00:35:17 +0900

    詳細画面でtodoの値表示するよう更新

--- lib/View/TodoDetailsScreen.dart
+++ lib/View/TodoDetailsScreen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:todolist/model/TodoModel.dart';
 
 class TodoDetailsScreen extends StatefulWidget {
-  TodoDetailsScreen({Key key}) : super(key: key);
+  final TodoModel todoModel;
+
+  TodoDetailsScreen({Key key, this.todoModel}) : super(key: key);
 
   @override
   _TodoDetailsScreen createState() => _TodoDetailsScreen();
@@ -14,7 +17,23 @@ class _TodoDetailsScreen extends State<TodoDetailsScreen> {
       appBar: AppBar(
         title: Text("詳細"),
       ),
-      body: Text("詳細画面"),
+      body: Container(
+        child: Column(
+          children: [
+            _valueRow("タイトル", widget.todoModel.title),
+            _valueRow("期限", widget.todoModel.date),
+            _valueRow("詳細", widget.todoModel.detail),
+          ],
+        ),
+      ),
     );
   }
+
+  Widget _valueRow(String title, String value) => Row(
+        children: [
+          Column(
+            children: [Text(title), Text(value)],
+          ),
+        ],
+      );
 }
